8d4be6a76aa73bca3b134eeda76eeb554d5bf626
RDarrel <116413862+RDarrel@users.noreply.github.com>
2024-12-16 04:45:54 +0800

    update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}", 
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       fontFamily: {
-        oswald: ['Oswald', 'sans-serif'],
+        oswald: ["Oswald", "sans-serif"],
+      },
+
+      fontFamily: {
+        custom: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          '"Roboto"',
+          '"Oxygen"',
+          '"Ubuntu"',
+          '"Cantarell"',
+          '"Fira Sans"',
+          '"Droid Sans"',
+          '"Helvetica Neue"',
+          "sans-serif",
+        ],
       },
     },
   },
   plugins: [],
-}
-
+};
